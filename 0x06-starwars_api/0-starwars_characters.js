786b7b6e6815335fcd1f4743857169f1f2af172f
Kamgo N Elton <kamgoelton89@gmail.com>
2024-04-17 18:14:02 +0100

    starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, i) => {
+  if (i === actors.length) return;
+  request(actors[i], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, i + 1);
+  });
+}
